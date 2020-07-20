9a598c32351a4430b8edb4f882ba3d2ea86948f8
selfsigned <selfsigned@protonmail.ch>
2020-07-20 19:08:18 +0200

    Json and quiet, todo filepath.Walk

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -10,6 +11,17 @@ import (
 	"github.com/selfsigned/timesink/ffprobe"
 )
 
+type parameters struct {
+	recursive bool
+	quiet     bool
+	json      bool
+}
+
+const (
+	usageText = `Usage: timesink [OPTION] [FILE]
+Estimate how long it'd take to consume the medias in a directory`
+)
+
 func toTimecode(d float64) string {
 	hr := int(d) / 3600
 	min := (int(d) - hr*3600) / 60
@@ -17,16 +29,35 @@ func toTimecode(d float64) string {
 	return fmt.Sprintf("%.2d:%.2d:%.2d", hr, min, sec)
 }
 
+func getFileLength(f string) (length float64) {
+	fileInfo, err := ffprobe.Exec(f)
+	if err == nil {
+		if fileInfo.Format.Duration != "" {
+			length, err = strconv.ParseFloat(fileInfo.Format.Duration, 64)
+			if err != nil {
+				fmt.Printf("%#v", fileInfo)
+				println(err.Error())
+			}
+			return
+		}
+	}
+	return
+}
+
 func main() {
 	var totalDuration float64
-	// var recurse = flag.Bool("recursive", false, "Recursively traverse folders")
+	var params parameters
 
-	// flag.BoolVar(recurse, "R", false, "(shorthand)")
+	flag.BoolVar(&params.recursive, "recursive", false, "Recursively traverse folders")
+	flag.BoolVar(&params.quiet, "quiet", false, "quieter output")
+	flag.BoolVar(&params.json, "json", false, "json output")
+	flag.BoolVar(&params.recursive, "R", false, "recursive (shorthand)")
+	flag.BoolVar(&params.quiet, "q", false, "quiet (shorthand)")
 	flag.Parse()
 
 	files := flag.Args()
 	if len(files) == 0 {
-		println("Usage: timesink [options] files")
+		println(usageText)
 		os.Exit(1)
 	}
 
@@ -37,30 +68,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	// WIP refactor this shit
+	// WIP better error handling
 	for _, f := range files {
-		println(f)
-		fileInfo, err := ffprobe.Exec(f)
-		if err == nil {
-			if fileInfo.Format.Duration != "" {
-				fileDuration, err := strconv.ParseFloat(fileInfo.Format.Duration, 32)
-				if err != nil {
-					fmt.Printf("%#v", fileInfo)
-					println(err.Error())
-					os.Exit(1)
-				}
-				if fileDuration > 1 {
-					totalDuration += fileDuration
-				}
-			}
+		fileDuration := getFileLength(f)
+		totalDuration += fileDuration
+		if !params.quiet && fileDuration != 0 {
+			println(f + " ⏳" + toTimecode(fileDuration))
 		}
 	}
+
 	if totalDuration < 1 {
 		println("Error: No valid media file found")
 		os.Exit(1)
 	}
 
-	fmt.Printf("\nDate after completion:\t%s\nTotal duration:\t\t%s\n",
-		time.Now().Add(time.Second*time.Duration(totalDuration)).Format(time.ANSIC),
-		toTimecode(totalDuration))
+	timeNow := time.Now()
+
+	output := struct {
+		Duration float64
+		TimeNow  time.Time
+		TimeEnd  time.Time
+	}{
+		totalDuration,
+		time.Now(),
+		timeNow.Add(time.Second * time.Duration(totalDuration)),
+	}
+
+	if params.json {
+		out, err := json.MarshalIndent(output, "", "	")
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("%s", string(out))
+	} else {
+		fmt.Printf("\nDate after completion:\t%s\nTotal duration:\t\t%s\n",
+			output.TimeEnd.Format(time.ANSIC),
+			toTimecode(output.Duration))
+	}
 }
